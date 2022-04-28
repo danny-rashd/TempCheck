b4b6fa5b8948137f8309ee35c974fe8b333acc0c
danny-rashd <danny.rashd@gmail.com>
2022-04-28 14:18:38 +0800

    Added LineChart.js and ChartData.js

---
+++ webapp/src/components/LineChart.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function LineChart() {
+  const labels = [];
+  const [data, setData] = useState({
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [],
+        borderColor: "rgb(255,99,132)",
+        backgroundColor: "rgb(255,99,132)",
+      },
+      {
+        label: "Dataset 2",
+        data: [],
+        borderColor: "rgb(53,162,235)",
+        backgroundColor: "rgb(53,162,235))",
+      },
+    ],
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      const url =
+        "https://raw.githubusercontent.com/danny-rashd/TempCheck/main/webapp/src/components/temp_final.json";
+      const labelSet = [];
+      const dataSet1 = [];
+      fetch(url, {
+        method: "GET",
+      })
+        .then((data) => {
+          console.log(data);
+          const res = data.json();
+          return res;
+        })
+        .then((res) => {
+          console.log(res);
+          for (const val of res) {
+            dataSet1.push(val.Temperature);
+            labelSet.push(val.Datetime);
+          }
+
+          setData({
+            labels: labelSet,
+            datasets: [
+              {
+                label: "Dataset 1",
+                data: dataSet1,
+                borderColor: "rgb(255,99,132)",
+                backgroundColor: "rgb(255,99,132)",
+              },
+            ],
+          });
+        })
+        .catch((e) => {
+          console.log("error", e);
+        });
+    };
+
+    fetchData();
+  });
+  return (
+    <div style={{ width: "70%", height: "70%" }}>
+      <Line data={data} />
+    </div>
+  );
+}
---
+++ webapp/src/pages/ChartData.js
@@ -0,0 +1,16 @@
+import LineChart from "../components/LineChart";
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function ChartData() {
+  return (
+    <div>
+      <Container className="d-flex justify-content-center mt-3">
+        <h1>ChartData</h1>
+      </Container>
+      <div className="justify-content-center d-flex"><LineChart /></div>
+    </div>
+  );
+}
+
+export default ChartData;
